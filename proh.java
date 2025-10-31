63df98c57bdf905fc3b7c9f0425313e5cffe77fa
diptyprakash776-lab <diptyprakash776@gmail.com>
2025-10-31 18:58:56 +0530

    Add files via upload

---
+++ proh.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class proh {
+
+    
+public static void main(String[] args) {
+    
+
+    int a,b,c,d;
+   
+        Scanner s =new Scanner(System.in);
+        a=s.nextInt();
+        b=s.nextInt();
+        c=s.nextInt();
+        d=a+b+c;
+        System.out.println(d);
+
+    }
+}
