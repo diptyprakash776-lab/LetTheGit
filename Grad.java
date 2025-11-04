4acde8715ab71d9ba9e195a5b830eebbd991cca5
DIPTY PRAKASH SWAIN <diptyprakash776@gmail.com>
2025-11-04 08:23:45 +0530

    Add files via upload

---
+++ Grad.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Grad {
+    public static void main(String[] args) {
+        int a;
+        Scanner e=new Scanner(System.in);
+        a=e.nextInt();
+        if (a>25) {
+            System.out.println("f");
+
+            
+        }
+       else if (a>30) {
+        System.out.println("e");
+        
+       }
+    }
+    
+}
